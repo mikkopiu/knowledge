3a322ccd7b2828fc84a87828eb3b9ced021385cd
Mikko Piuhola <mikkopiu@users.noreply.github.com>
2016-11-18 23:01:39 +0200

    Update createAllCerts.sh

--- security/self-signed-certs/createAllCerts.sh
+++ security/self-signed-certs/createAllCerts.sh
@@ -4,6 +4,10 @@ set -o xtrace errexit nounset
 
 : "${SERVER_NAME:?SERVER_NAME missing! Usage: \"SERVER_NAME=my-server sh createAllCerts.sh\"}"
 
+mkdir -p CA/intermediate
+cp openssl.cnf CA/
+cp openssl.intermediate.cnf CA/intermediate/openssl.cnf
+
 sh createRootCA.sh
 sh createIntermediateCA.sh
 sh createServerCert.sh
