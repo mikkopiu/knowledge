98218395c7d1e2b189edb8a3dda543cf56b4195c
Mikko Piuhola <mikkopiu@users.noreply.github.com>
2016-11-18 19:40:19 +0200

    Create createRootCA.sh

---
+++ security/self-signed-certs/createRootCA.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -o xtrace errexit
+
+# Create CA directories
+# and set necessary file permissions.
+mkdir -p ./CA/certs
+mkdir -p ./CA/crl
+mkdir -p ./CA/newcerts
+mkdir -p ./CA/private
+cd CA
+chmod 700 private
+
+# Create a flat file database for signed certificates
+touch index.txt
+echo 1000 > serial
+
+if [ -f ./openssl.cnf ];
+then
+    echo 'openssl.cnf is missing, please add it in the CA directory'
+    exit 1
+fi
+
+# Generate a private key,
+# and make sure it is set to read-only and for only the current user.
+openssl genrsa -aes256 -out private/CA.key.pem 4096
+chmod 400 private/CA.key.pem
+
+# Create the actual certificate, see README for more info
+openssl req -config openssl.cnf -key private/CA.key.pem -new -x509 -days 3650 -sha256 -extensions v3_ca -out certs/CA.cert.pem
+chmod 444 certs/CA.cert.pem
+
+echo 'Done'
