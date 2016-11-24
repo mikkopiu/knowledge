319ecc2d390fa37ac9f172967ce1727adcc69869
Mikko Piuhola <mikko.piuhola@gmail.com>
2016-11-24 21:16:04 +0200

    Clean up self-signing proxy

--- docker/auto-proxy/genCert.sh
+++ docker/auto-proxy/genCert.sh
@@ -7,7 +7,7 @@ CERT_DIR="certs"
 CERT_FNAME="$CERT_DIR/$CERT_VIRTUAL_HOST"
 mkdir -p $CERT_DIR
 
-openssl req -x509 -nodes -newkey rsa:2048 -keyout $CERT_FNAME.key -out $CERT_FNAME.crt -days 365 -passin env:CERT_PASSWORD -subj $CERT_SUBJ
+openssl req -x509 -nodes -newkey rsa:2048 -keyout $CERT_FNAME.key -days 365 -passin env:CERT_PASSWORD -subj "$CERT_SUBJ" -out $CERT_FNAME.crt
 chmod go-rwx $CERT_FNAME.key
 
 echo "Generated certificate in $CERT_DIR"
