f1b46d89fc09a43ce2c8fa71e70484aeb46fdde3
Mikko Piuhola <me@mikkopiuhola.fi>
2019-03-05 13:35:21 +0200

    Add u2f login setup

---
+++ linux/u2f/setup-login.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+
+echo 'Setting up Yubico repo and installing required PAM libs...'
+sudo add-apt-repository ppa:yubico/stable && sudo apt-get update
+sudo apt-get install libpam-u2f
+
+echo 'Associating Yubikey with your account...'
+echo 'Touch the device when it blinks'
+mkdir -p ~/.config/Yubico
+pamu2fcfg > ~/.config/Yubico/u2f_keys
+
+echo 'Configuring login, via gdm-password...'
+sed '/@include common-auth/a auth\trequired\tpam_u2f.so' /etc/pam.d/gdm-password
+
+echo 'Done!'
+
